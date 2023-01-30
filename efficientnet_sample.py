254f28d88a6100f3fc4459222ebf5e1984040da0
ally4n <94357446+ally4n@users.noreply.github.com>
2023-01-30 17:51:58 +0300

    Update efficientnet_sample.py

--- efficientnet_sample.py
+++ efficientnet_sample.py
@@ -1,163 +1,166 @@
 from __future__ import print_function, division
 
-def main():
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torchvision import datasets, models, transforms
+import time
+import os
+from efficientnet.model import EfficientNet
 
-    import torch
-    import torch.nn as nn
-    import torch.optim as optim
-    from torch.autograd import Variable
-    from torchvision import datasets, models, transforms
-    import time
-    import os
-    from efficientnet.model import EfficientNet
-
-    # some parameters
-    use_gpu = torch.cuda.is_available()
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-    data_dir = '//kaggle//input//imagenetbirads//dataset//'
-    batch_size = 1
-    lr = 0.01
-    momentum = 0.9
-    num_epochs = 60
-    input_size = 1024
-    class_num = 3
-    net_name = 'efficientnet-b5'
-
-    def loaddata(data_dir, batch_size, set_name, shuffle):
-        data_transforms = {
-            'train': transforms.Compose([
-                transforms.Resize(input_size),
-                transforms.CenterCrop(input_size),
-                transforms.RandomAffine(degrees=0, translate=(0.05, 0.05)),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-            ]),
-            'test': transforms.Compose([
-                transforms.Resize(input_size),
-                transforms.CenterCrop(input_size),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-            ]),
-        }
-
-        image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in [set_name]}
-        # num_workers=0 if CPU else =1
-        dataset_loaders = {x: torch.utils.data.DataLoader(image_datasets[x],
-                                                        batch_size=batch_size,
-                                                        shuffle=shuffle, num_workers=1) for x in [set_name]}
-        data_set_sizes = len(image_datasets[set_name])
-        return dataset_loaders, data_set_sizes
+# some parameters
+use_gpu = torch.cuda.is_available()
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+data_dir = '//kaggle//input//imagenetbirads//dataset//'
+batch_size = 8
+lr = 0.01
+momentum = 0.9
+num_epochs = 60
+input_size = 1024
+class_num = 3
+net_name = 'efficientnet-b5'
 
 
-    def train_model(model_ft, criterion, optimizer, lr_scheduler, num_epochs=50):
-        train_loss = []
-        since = time.time()
-        best_model_wts = model_ft.state_dict()
-        best_acc = 0.0
-        model_ft.train(True)
-        for epoch in range(num_epochs):
-            dset_loaders, dset_sizes = loaddata(data_dir=data_dir, batch_size=batch_size, set_name='train', shuffle=True)
-            print('Data Size', dset_sizes)
-            print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-            print('-' * 10)
-            optimizer = lr_scheduler(optimizer, epoch)
+def loaddata(data_dir, batch_size, set_name, shuffle):
+    data_transforms = {
+        'train': transforms.Compose([
+            transforms.Resize(input_size),
+            transforms.CenterCrop(input_size),
+            transforms.RandomAffine(degrees=0, translate=(0.05, 0.05)),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+        'test': transforms.Compose([
+            transforms.Resize(input_size),
+            transforms.CenterCrop(input_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ]),
+    }
 
-            running_loss = 0.0
-            running_corrects = 0
-            count = 0
-
-            for data in dset_loaders['train']:
-                inputs, labels = data
-                labels = torch.squeeze(labels.type(torch.LongTensor))
-                if use_gpu:
-                    inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
-                else:
-                    inputs, labels = Variable(inputs), Variable(labels)
-
-                outputs = model_ft(inputs)
-                loss = criterion(outputs, labels)
-                _, preds = torch.max(outputs.data, 1)
-
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-
-                count += 1
-                if count % 30 == 0 or outputs.size()[0] < batch_size:
-                    print('Epoch:{}: loss:{:.3f}'.format(epoch, loss.item()))
-                    train_loss.append(loss.item())
-
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-
-            epoch_loss = running_loss / dset_sizes
-            epoch_acc = running_corrects.double() / dset_sizes
-
-            print('Loss: {:.4f} Acc: {:.4f}'.format(
-                epoch_loss, epoch_acc))
-
-            if epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = model_ft.state_dict()
-            if epoch_acc > 0.999:
-                break
-
-        # save best model
-        save_dir = data_dir + '/model'
-        model_ft.load_state_dict(best_model_wts)
-        model_out_path = save_dir + "/" + net_name + 'pth'
-        torch.save(model_ft, model_out_path)
-
-        time_elapsed = time.time() - since
-        print('Training complete in {:.0f}m {:.0f}s'.format(
-            time_elapsed // 60, time_elapsed % 60))
-
-        return train_loss, best_model_wts
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in [set_name]}
+    # num_workers=0 if CPU else =1
+    dataset_loaders = {x: torch.utils.data.DataLoader(image_datasets[x],
+                                                      batch_size=batch_size,
+                                                      shuffle=shuffle, num_workers=1) for x in [set_name]}
+    data_set_sizes = len(image_datasets[set_name])
+    return dataset_loaders, data_set_sizes
 
 
-    def test_model(model, criterion):
-        model.eval()
+def train_model(model_ft, criterion, optimizer, lr_scheduler, num_epochs=50):
+    train_loss = []
+    since = time.time()
+    best_model_wts = model_ft.state_dict()
+    best_acc = 0.0
+    model_ft.train(True)
+    for epoch in range(num_epochs):
+        dset_loaders, dset_sizes = loaddata(data_dir=data_dir, batch_size=batch_size, set_name='train', shuffle=True)
+        print('Data Size', dset_sizes)
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+        optimizer = lr_scheduler(optimizer, epoch)
+
         running_loss = 0.0
         running_corrects = 0
-        cont = 0
-        outPre = []
-        outLabel = []
-        dset_loaders, dset_sizes = loaddata(data_dir=data_dir, batch_size=16, set_name='test', shuffle=False)
-        for data in dset_loaders['test']:
+        count = 0
+
+        for data in dset_loaders['train']:
             inputs, labels = data
             labels = torch.squeeze(labels.type(torch.LongTensor))
-            inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
-            outputs = model(inputs)
-            _, preds = torch.max(outputs.data, 1)
-            loss = criterion(outputs, labels)
-            if cont == 0:
-                outPre = outputs.data.cpu()
-                outLabel = labels.data.cpu()
+            print(labels)
+            if use_gpu:
+                inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
             else:
-                outPre = torch.cat((outPre, outputs.data.cpu()), 0)
-                outLabel = torch.cat((outLabel, labels.data.cpu()), 0)
+                inputs, labels = Variable(inputs), Variable(labels)
+
+            outputs = model_ft(inputs)
+            print(outputs)
+            print(labels)
+
+            loss = criterion(outputs, labels)
+            _, preds = torch.max(outputs.data, 1)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            count += 1
+            if count % 30 == 0 or outputs.size()[0] < batch_size:
+                print('Epoch:{}: loss:{:.3f}'.format(epoch, loss.item()))
+                train_loss.append(loss.item())
+
             running_loss += loss.item() * inputs.size(0)
             running_corrects += torch.sum(preds == labels.data)
-            cont += 1
-        print('Loss: {:.4f} Acc: {:.4f}'.format(running_loss / dset_sizes,
-                                                running_corrects.double() / dset_sizes))
+
+        epoch_loss = running_loss / dset_sizes
+        epoch_acc = running_corrects.double() / dset_sizes
+
+        print('Loss: {:.4f} Acc: {:.4f}'.format(
+            epoch_loss, epoch_acc))
+
+        if epoch_acc > best_acc:
+            best_acc = epoch_acc
+            best_model_wts = model_ft.state_dict()
+        if epoch_acc > 0.999:
+            break
+
+    # save best model
+    save_dir = data_dir + '/model'
+    model_ft.load_state_dict(best_model_wts)
+    model_out_path = save_dir + "/" + net_name + 'pth'
+    torch.save(model_ft, model_out_path)
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+
+    return train_loss, best_model_wts
 
 
-    def exp_lr_scheduler(optimizer, epoch, init_lr=0.01, lr_decay_epoch=10):
-        """Decay learning rate by a f#            model_out_path ="./model/W_epoch_{}.pth".format(epoch)
-    #            torch.save(model_W, model_out_path) actor of 0.1 every lr_decay_epoch epochs."""
-        lr = init_lr * (0.8**(epoch // lr_decay_epoch))
-        print('LR is set to {}'.format(lr))
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
+def test_model(model, criterion):
+    model.eval()
+    running_loss = 0.0
+    running_corrects = 0
+    cont = 0
+    outPre = []
+    outLabel = []
+    dset_loaders, dset_sizes = loaddata(data_dir=data_dir, batch_size=16, set_name='test', shuffle=False)
+    for data in dset_loaders['test']:
+        inputs, labels = data
+        labels = torch.squeeze(labels.type(torch.LongTensor))
+        inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+        outputs = model(inputs)
+        _, preds = torch.max(outputs.data, 1)
+        loss = criterion(outputs, labels)
+        if cont == 0:
+            outPre = outputs.data.cpu()
+            outLabel = labels.data.cpu()
+        else:
+            outPre = torch.cat((outPre, outputs.data.cpu()), 0)
+            outLabel = torch.cat((outLabel, labels.data.cpu()), 0)
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
+        cont += 1
+    print('Loss: {:.4f} Acc: {:.4f}'.format(running_loss / dset_sizes,
+                                            running_corrects.double() / dset_sizes))
 
-        return optimizer
 
+def exp_lr_scheduler(optimizer, epoch, init_lr=0.01, lr_decay_epoch=10):
+    """Decay learning rate by a f#            model_out_path ="./model/W_epoch_{}.pth".format(epoch)
+#            torch.save(model_W, model_out_path) actor of 0.1 every lr_decay_epoch epochs."""
+    lr = init_lr * (0.8**(epoch // lr_decay_epoch))
+    print('LR is set to {}'.format(lr))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
 
+    return optimizer
+
+def run():
     # train
     pth_map = {
-        'efficientnet-b0': '/efficientnet-b0-355c32eb.pth',
+        'efficientnet-b0': 'efficientnet-b0-355c32eb.pth',
         'efficientnet-b1': 'efficientnet-b1-f1951068.pth',
         'efficientnet-b2': 'efficientnet-b2-8bb594d6.pth',
         'efficientnet-b3': 'efficientnet-b3-5fb5a3c3.pth',
@@ -166,9 +169,12 @@ def main():
         'efficientnet-b6': 'efficientnet-b6-c76e70fd.pth',
         'efficientnet-b7': 'efficientnet-b7-dcc49843.pth',
     }
+
     # Automatically download to local pre-training
-    model_ft = EfficientNet.from_pretrained(net_name)
+    model_ft = EfficientNet.from_pretrained('efficientnet-b3')
+
     # Offline loading pre-training needs to be downloaded in advance
+
     #model_ft = EfficientNet.from_name(net_name)
     #net_weight = 'eff_weights/' + pth_map[net_name]
     #state_dict = torch.load(net_weight)
@@ -195,6 +201,5 @@ def main():
     criterion = nn.CrossEntropyLoss().cuda()
     test_model(model_ft, criterion)
 
-
 if __name__ == '__main__':
-    main()
+    run()
