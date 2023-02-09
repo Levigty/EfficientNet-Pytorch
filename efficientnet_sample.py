ac906a7252b78e920ffcbcf692dc08cd6a4c2077
ally4n <94357446+ally4n@users.noreply.github.com>
2023-02-09 03:28:52 +0300

    Added parser arguments for easier custom training.

--- efficientnet_sample.py
+++ efficientnet_sample.py
@@ -9,17 +9,24 @@ import time
 import os
 from efficientnet.model import EfficientNet
 
+import argparse
+
 # some parameters
 use_gpu = torch.cuda.is_available()
+print(use_gpu)
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-data_dir = 'Flowers'
-batch_size = 64
+
+data_dir = ''
+num_epochs = 40
+batch_size = 2
+input_size = 4
+class_num = 3
+weights_loc = ""
 lr = 0.01
+net_name = 'efficientnet-b3'
+epoch_to_resume_from = 0
 momentum = 0.9
-num_epochs = 60
-input_size = 224
-class_num = 17
-net_name = 'efficientnet-b0'
 
 
 def loaddata(data_dir, batch_size, set_name, shuffle):
@@ -50,12 +57,15 @@ def loaddata(data_dir, batch_size, set_name, shuffle):
 
 
 def train_model(model_ft, criterion, optimizer, lr_scheduler, num_epochs=50):
+
     train_loss = []
     since = time.time()
     best_model_wts = model_ft.state_dict()
     best_acc = 0.0
     model_ft.train(True)
-    for epoch in range(num_epochs):
+
+    for epoch in range(epoch_to_resume_from, num_epochs):
+
         dset_loaders, dset_sizes = loaddata(data_dir=data_dir, batch_size=batch_size, set_name='train', shuffle=True)
         print('Data Size', dset_sizes)
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
@@ -75,6 +85,11 @@ def train_model(model_ft, criterion, optimizer, lr_scheduler, num_epochs=50):
                 inputs, labels = Variable(inputs), Variable(labels)
 
             outputs = model_ft(inputs)
+            
+            if count % 500 == 0:
+                print(outputs)
+                print(labels)
+            
             loss = criterion(outputs, labels)
             _, preds = torch.max(outputs.data, 1)
 
@@ -105,7 +120,7 @@ def train_model(model_ft, criterion, optimizer, lr_scheduler, num_epochs=50):
     # save best model
     save_dir = data_dir + '/model'
     model_ft.load_state_dict(best_model_wts)
-    model_out_path = save_dir + "/" + net_name + 'pth'
+    model_out_path = save_dir + "/" + net_name + '.pth'
     torch.save(model_ft, model_out_path)
 
     time_elapsed = time.time() - since
@@ -122,7 +137,7 @@ def test_model(model, criterion):
     cont = 0
     outPre = []
     outLabel = []
-    dset_loaders, dset_sizes = loaddata(data_dir=data_dir, batch_size=16, set_name='test', shuffle=False)
+    dset_loaders, dset_sizes = loaddata(data_dir=data_dir, batch_size=batch_size, set_name='test', shuffle=False)
     for data in dset_loaders['test']:
         inputs, labels = data
         labels = torch.squeeze(labels.type(torch.LongTensor))
@@ -154,42 +169,94 @@ def exp_lr_scheduler(optimizer, epoch, init_lr=0.01, lr_decay_epoch=10):
     return optimizer
 
 
-# train
-pth_map = {
-    'efficientnet-b0': 'efficientnet-b0-355c32eb.pth',
-    'efficientnet-b1': 'efficientnet-b1-f1951068.pth',
-    'efficientnet-b2': 'efficientnet-b2-8bb594d6.pth',
-    'efficientnet-b3': 'efficientnet-b3-5fb5a3c3.pth',
-    'efficientnet-b4': 'efficientnet-b4-6ed6700e.pth',
-    'efficientnet-b5': 'efficientnet-b5-b6417697.pth',
-    'efficientnet-b6': 'efficientnet-b6-c76e70fd.pth',
-    'efficientnet-b7': 'efficientnet-b7-dcc49843.pth',
-}
-# 自动下载到本地预训练
-# model_ft = EfficientNet.from_pretrained('efficientnet-b0')
-# 离线加载预训练，需要事先下载好
-model_ft = EfficientNet.from_name(net_name)
-net_weight = 'eff_weights/' + pth_map[net_name]
-state_dict = torch.load(net_weight)
-model_ft.load_state_dict(state_dict)
+def run():
+    # train
+    pth_map = {
+        'efficientnet-b0': 'efficientnet-b0-355c32eb.pth',
+        'efficientnet-b1': 'efficientnet-b1-f1951068.pth',
+        'efficientnet-b2': 'efficientnet-b2-8bb594d6.pth',
+        'efficientnet-b3': 'efficientnet-b3-5fb5a3c3.pth',
+        'efficientnet-b4': 'efficientnet-b4-6ed6700e.pth',
+        'efficientnet-b5': 'efficientnet-b5-b6417697.pth',
+        'efficientnet-b6': 'efficientnet-b6-c76e70fd.pth',
+        'efficientnet-b7': 'efficientnet-b7-dcc49843.pth',
+    }
 
-# 修改全连接层
-num_ftrs = model_ft._fc.in_features
-model_ft._fc = nn.Linear(num_ftrs, class_num)
 
-criterion = nn.CrossEntropyLoss()
-if use_gpu:
-    model_ft = model_ft.cuda()
-    criterion = criterion.cuda()
 
-optimizer = optim.SGD((model_ft.parameters()), lr=lr,
-                      momentum=momentum, weight_decay=0.0004)
+    if weights_loc != None:
+        model_ft = torch.load(weights_loc)
+    else:
+        model_ft = EfficientNet.from_pretrained(net_name)
 
-train_loss, best_model_wts = train_model(model_ft, criterion, optimizer, exp_lr_scheduler, num_epochs=num_epochs)
 
-# test
-print('-' * 10)
-print('Test Accuracy:')
-model_ft.load_state_dict(best_model_wts)
-criterion = nn.CrossEntropyLoss().cuda()
-test_model(model_ft, criterion)
+    # Modify the fully connected layer
+    num_ftrs = model_ft._fc.in_features
+    model_ft._fc = nn.Linear(num_ftrs, class_num)
+
+    criterion = nn.CrossEntropyLoss()
+
+    if use_gpu:
+        model_ft = model_ft.cuda()
+        criterion = criterion.cuda()
+
+    optimizer = optim.SGD((model_ft.parameters()), lr=lr,
+                        momentum=momentum, weight_decay=0.0004)
+
+    train_loss, best_model_wts = train_model(model_ft, criterion, optimizer, exp_lr_scheduler, num_epochs=num_epochs)
+
+    # test
+    print('-' * 10)
+    print('Test Accuracy:')
+
+    model_ft.load_state_dict(best_model_wts)
+
+    criterion = nn.CrossEntropyLoss().cuda()
+
+    test_model(model_ft, criterion)
+
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--data-dir', type=str, default=None, help='path of /dataset/')
+    parser.add_argument('--num-epochs', type=int, default=40)
+    parser.add_argument('--batch-size', type=int, default=4, help='total batch size for all GPUs')
+    parser.add_argument('--img-size', type=int, default=[1024, 1024], help='img sizes')
+    parser.add_argument('--class-num', type=int, default=3, help='class num')
+
+    parser.add_argument('--weights-loc', type=str, default= None, help='path of weights (if going to be loaded)')
+
+    parser.add_argument("--lr", type=float, default= 0.01, help="learning rate")
+    parser.add_argument("--net-name", type=str, default="efficientnet-b3", help="efficientnet type")
+
+    parser.add_argument('--resume-epoch', type=int, default=0, help='what epoch to start from')
+
+    parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
+
+
+
+    opt = parser.parse_args()
+
+    data_dir = opt.data_dir
+    num_epochs = opt.num_epochs
+    batch_size = opt.batch_size
+    input_size = opt.img_size
+    class_num = opt.class_num
+
+    weights_loc = opt.weights_loc
+
+    lr = opt.lr
+    net_name = opt.net_name
+
+    epoch_to_resume_from = opt.resume_epoch
+
+    momentum = opt.momentum
+
+    print("data dir: ", data_dir, ",  num epochs: ", num_epochs, ",  batch size: ",batch_size,
+             ", img size: ", input_size, ", num of classes:", class_num, ".pth weights file location:", weights_loc,
+             ", learning rate:", lr, ", net name:", net_name, "epoch to resume from: ", epoch_to_resume_from, "momen")
+
+    run()
